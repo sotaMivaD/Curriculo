21e03cd3cb3c998ea0ecc8741d01a5964b0886d0
Davi Matos <davimatis577@gmail.com>
2026-04-20 12:27:00 -0300

    alteracoes 20/04/2026

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿function alternarSom() {
+  const audio = document.getElementById('meuAudio');
+  const icone = document.getElementById('iconeSom');
+
+  if (audio.paused) {
+    audio.play();
+    icone.innerHTML = "🔊"; // Muda para ícone de som tocando
+  } else {
+    audio.pause();
+    icone.innerHTML = "🔇"; // Muda para ícone de mudo
+  }
+}
